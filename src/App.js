2385ec26624061f93ed23eddbd5ac87c1329036a
Felipe Rodriguez <frodriguez@ecomsur.com>
2021-06-03 21:24:49 -0400

    limpiando react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+let name = "felipe";
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola soy {name}
         </p>
         <a
           className="App-link"
